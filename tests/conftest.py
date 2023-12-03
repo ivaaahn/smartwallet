ba48d84d5ce59300bd6ed6c644d4c11194d49b22
d_ivahnencko <ivahnencko01@gmail.com>
2023-12-03 14:12:33 +0300

    #1: fix lint issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,11 @@ from asyncio import AbstractEventLoop
 from collections.abc import AsyncIterator, Iterator
 
 import asyncpg
-import httpx
 import pytest
 from asyncpg import Connection
-from asyncpg.pool import PoolConnectionProxy
 from fastapi.testclient import TestClient
 
 from wallet.app import app
-from wallet.apps.auth.entities import UserDC
 from wallet.base.config import Settings, settings_factory
 
 client = TestClient(app)
@@ -43,29 +40,3 @@ async def conn(settings: Settings) -> AsyncIterator[Connection]:
             f"TRUNCATE {table["tablename"]} RESTART IDENTITY CASCADE"
         )
     await conn.close()
-
-
-@pytest.fixture
-async def user(conn: PoolConnectionProxy) -> UserDC:
-    # INFO: pwd: 123456
-    user = UserDC(
-        name="Вася",
-        email="vvv@mail.ru",
-        hashed_password="$2b$12$Y/NpUBfgqDUm8026j1FHPOK9dVcoSgi974LIc1FNBy5NC5wl/0iye",
-    )
-    await conn.execute(
-        "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)",
-        user.name,
-        user.email,
-        user.hashed_password,
-    )
-    return user
-
-
-class BearerAuth:
-    def __init__(self, token: str) -> None:
-        self._token = token
-
-    def __call__(self, request: httpx.Request) -> httpx.Request:
-        request.headers["Authorization"] = f"Bearer {self._token}"
-        return request
